90bf1784734c0ccffe6abff66a9aa452c78001ef
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-11-09 19:13:06 -0300

    ✅📄 finish swagger challenge

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerDoc from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -7,5 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export { app };
